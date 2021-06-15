edec4b72dcde275b1bbbe6959b9ceb6264facfdf
Juan Luis Cano Rodríguez <jcano@faculty.ie.edu>
2021-06-15 11:31:33 +0200

    Add tests and lower parameter to tokenize

--- src/ie_utils/__init__.py
+++ src/ie_utils/__init__.py
@@ -6,7 +6,9 @@ __version__ = "0.1.0"  # semver.org
 
 import pandas as pd
 
-def tokenize(text):
+def tokenize(text, lower=False):
+    if lower:
+        text = text.lower()
     return text.split()
 
 if __name__ == "__main__":
