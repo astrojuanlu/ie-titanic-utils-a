a28ce8dbe3741b5327380297a26f3be109a9269b
Juan Luis Cano Rodríguez <jcano@faculty.ie.edu>
2021-06-25 11:39:28 +0200

    Move app settings to environment

--- app.py
+++ app.py
@@ -15,3 +15,9 @@ def do_tokenize():
     sentence = request.args["sentence"]
     lower = bool(request.args.get("lower", False))
     return str(tokenize(sentence, lower=lower))
+
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ["PORT"])
+    app.run(host="0.0.0.0", port=port)
